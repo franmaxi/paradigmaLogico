3b42f3ef25b7960f8cb34b46dadfe2f97d375712
franmaxi <franmaxii03@gmail.com>
2025-08-26 00:56:17 -0300

    Haciendo finales

--- Finales/pruebas_final.pl
+++ Finales/pruebas_final.pl
@@ -148,41 +148,263 @@
 % marcaPopu1(Marca):-forall(cliente(Cliente),comproMarca(Cliente, Marca)).
 % marcaPopu2(Marca):-cliente(Cliente),forall(cliente(Cliente),comproMarca(Cliente, Marca)).
 
-%genero(Canción, Género).
-genero(el38, rock).
-% genero(sisters, reggae).
-genero(muchoPorHacer, rock).
-genero(tusojos, reggae).
-genero(bastara, reggae).
+% %genero(Canción, Género).
+% genero(el38, rock).
+% % genero(sisters, reggae).
+% genero(muchoPorHacer, rock).
+% genero(tusojos, reggae).
+% genero(bastara, reggae).
 
-%toca (Tema, Banda).
-toca(el38, divididos).
-toca(sisters, divididos).
-toca(muchoPorHacer, riff).
-toca(tusojos, losCafres).
-toca(bastara, losCafres).
+% %toca (Tema, Banda).
+% toca(el38, divididos).
+% toca(sisters, divididos).
+% toca(muchoPorHacer, riff).
+% toca(tusojos, losCafres).
+% toca(bastara, losCafres).
 
-rockera(Banda):-
-    findall(Tema,(toca(Tema,Banda),genero(Tema,Genero),Genero \= rock),Temas),
-    length(Temas, 0).
+% rockera(Banda):-
+%     findall(Tema,(toca(Tema,Banda),genero(Tema,Genero),Genero \= rock),Temas),
+%     length(Temas, 0).
+
+% /*
+%     rockera(riff).
+%     true
+%     rockera(divididos).
+%     false
+%     rockera(42).
+%     false -> Da True
+%     rockera(Banda)
+%     false
+
+%     No funciona correctamente la consulta existencial debido a que la variable "Banda" es usada
+%     por primera vez en un predicado no inversible, por lo tanto rockera no puede generar el universo
+%     de Bandas. Ademas rockera(42) da True ya que la longitud de sus canciones es igual a cero, (por mas que no sea una banda)
+
+
+% */
+
+% rockera1(Banda):-
+%     toca(Tema,Banda),
+%     not((toca(OtroTema,Banda),genero(OtroTema,Genero),Genero\= rock )).
+% estaEn(Peli, Personaje)
+% estaEn(buscandoNemo, nemo).
+% estaEn(buscandoNemo, dory).
+% estaEn(quienEnganio, rogerRabbit).
+% estaEn(quienEnganio, doom).
+% estaEn(quienEnganio, eddie).
+% estaEn(naufrago, chuck).
+
+% % esAnimado(Personaje)
+% esAnimado(nemo).
+% esAnimado(dory).
+% esAnimado(rogerRabbit).
+% esAnimado(doom). %Ups, spoiler
+
+% aptaParaAmargos(Peli):-
+%     findall(Pers, (estaEn(Peli, Pers), esAnimado(Pers)), Ps),
+%     length(Ps, C), C = 0.
+
+% seDivierte(Personaje):-
+%     findall(Animado,
+%         (estaEn(Peli1, Personaje),
+%          estaEn(Peli2, Animado),
+%          Peli1 = Peli2,
+%          esAnimado(Animado)),
+%         Compas),
+%     length(Compas, C), C >= 1.
+
+% /**
+%  1- Una pelicula es apta para amargos cuando una pelicula no contiene personajes animados
+%     Un personaje se divierte, cuando forma parte de una pelicula donde al menos haya un personaje que si es animado
+%     aptaParaAmargos(naufrago).
+%     true
+%     aptaParaAmargos(nemo).
+%     false
+%     seDivierte(nemo).
+%     true (participa con dory)
+%     seDivierte(chuck).
+%     false
+
+%     aptaParaAmargos(Pelicula).
+%     false
+
+%     seDiverite(Personaje).
+%     true
+
+%     Sucede esto debido a que el predicado no es inversible, por ende no tiene un para generar una respuesta.
+
+
+%     En seDivierte estamos teniendo un problema de pattern mattching ya que estamos creando dos variables diferentes que tienen
+%     que ser iguales, lo cual no tiene sentido porque podriamos usar directamente la misma variable para hacer la consulta.
+
+%     Mientras tanto las dos tienen problemas de declaratividad, ya que estamos creando listas y utilizando predicados de orden superior
+%     cuando no es necesario. 
+
+%     Ademas de que el predicado aptaParaAmargos no funciona correctamente, ya que le pondiramos preguntar por pepe 
+%     y diria que es true.(Esto se cumple debido a la linea donde C = 0, como pepe no existe, entonoces siempre va a ser cero la longitud).
+%  **/
+
+% aptaParaAmargos1(Pelicula):-
+%     estaEn(Pelicula,_),
+%     not((estaEn(Pelicula,Personaje),esAnimado(Personaje))).
+
+% seDivierte1(Personaje):-
+%     estaEn(Pelicula,Personaje),
+%     estaEn(Pelicula,OtroPersonaje),
+%     OtroPersonaje \= Personaje,
+%     esAnimado(OtroPersonaje).
 
 /*
-    rockera(riff).
-    true
-    rockera(divididos).
-    false
-    rockera(42).
-    false -> Da True
-    rockera(Banda)
-    false
+    Se puede anotar a un final cuando aprobo la cursada , no aprobo el final de esa materia y aprobo
+    todo los finales de las correlativas, a menos que haya pasado menos de un año lectivo
+    desde que aprobo la cursada, en cuyo caso las correlatividades de final no aplican
+*/
 
-    No funciona correctamente la consulta existencial debido a que la variable "Banda" es usada
-    por primera vez en un predicado no inversible, por lo tanto rockera no puede generar el universo
-    de Bandas. Ademas rockera(42) da True ya que la longitud de sus canciones es igual a cero, (por mas que no sea una banda)
+
+% puedeAnotarseAFinal(Alumno, Materia, Fecha):-
+%     aproboCursada(Alumno, Materia, FechaFirma),
+%     not(aproboFinal(Alumno, Materia, _)),
+%     añosLectivosTranscurridos(FechaFirma, Fecha, 0).
+
+% puedeAnotarseAFinal(Alumno, Materia, _):-
+%     aproboCursada(Alumno, Materia, _),
+%     not(aproboFinal(Alumno, Materia, _)),
+%     correlativa(Correlativa, Materia),
+%     aproboFinal(Alumno, Correlativa, _).
+
+% aproboCursada(Alumno,Materia,FechaCursada). Inverisble
+% aproboFinal(Alumno,Materia,FechaCursada). Inversible
+% correlativa(MateriaPrevia,MateriaDespues). Es inversible
+% añosLectivosTranscurridos(FechaFirma,FechaActual,Diferencia). Es inverisble para el tercer parametro
+
+/*
+    Esta mal la logica debido a que esta fijando que al menos haya aprobado un final de la correlativa,
+    por ejemplo si tenemos una materia que para es correlativa de 3 materias, si al menos aprobo un final de esas
+    3 estaria cumpliendo, pero para anotarse al final tiene que haber aprobado los 3 finales.
+
+    Es inverisble para el parametro Fecha.
+
 
 
 */
 
-rockera1(Banda):-
-    toca(Tema,Banda),
-    not((toca(OtroTema,Banda),genero(OtroTema,Genero),Genero\= rock )).
+% puedeAnotarseAFinal1(Alumno,Materia,Fecha):-
+%     requerimientosMinimos(Alumno,Materia,FechaFirma),
+%     añosLectivosTranscurridos(FechaFirma, Fecha, Diferencia),
+%     Diferencia =< 1.
+
+% puedeAnotarseAFinal(Alumno, Materia, _):-
+%     requerimientosMinimos(Alumno,Materia,_),
+%     forall(correlativas(Correlativa,Materia), aproboFinal(Alumno,Correlativa,_)).
+
+% requerimientosMinimos(Alumno,Materia,FechaFirma):-
+%     aproboCursada(Alumno,Materia,FechaFirma),
+%     not(aproboFinal(Alumno,Materia,_)).
+
+
+% tiene(Persona,Cosa).
+% vale(Cosa,Valor).
+
+% todoLoQueTieneEsMasValioso(Persona1,Persona2):-
+%     forall((
+%         tiene(Persona1,Cosa),tiene(Persona2,OtraCosa),
+%         vale(Cosa,ValorCosaValiosa),vale(OtraCosa,OtroValor)
+%         ),ValorCosaValiosa > OtroValor).
+
+%     /*
+%         No se pueden hacer consultas existenciales debido a que el predicado no es inversible, 
+%         si quisieramos realizar esa consulta tendriamos que modifcarlo
+
+
+%         not(todoLoQueTieneEsMasValioso1(_,pedro)).
+%         todoLoQueTieneEsMasValioso1(Persona,pedro).
+%     */
+
+%     todoLoQueTieneEsMasValioso1(Persona1,Persona2):-
+%     tiene(Persona1,_),tiene(Persona2,_),
+%     forall((
+%         tiene(Persona1,Cosa),tiene(Persona2,OtraCosa),
+%         vale(Cosa,ValorCosaValiosa),vale(OtraCosa,OtroValor)
+%         ),ValorCosaValiosa > OtroValor).
+
+
+% tiene(tito,arboles,5).
+% tiene(tito,problemas,2).
+% tiene(jorge,autos,8).
+% tiene(jorge,motos,2).
+% tiene(dalia,kilos,10).
+% tiene(dalia,problemas,9).
+
+% predicadoMisterioso(Persona,Cosa,CantidadM):-
+%     tiene(Persona,Cosa,CantidadM),
+%     forall(tiene(Persona,_,Cantidad),CantidadM >= Cantidad).
+
+% /*
+%     predicadoMisterioso(tito,Cosa,MayorCantidad).
+%     predicadoMisterioso(Persona,_,MayorCantidad).
+% */
+% mayorCantidadDeUnaCosa(Persona,Cosa,MayorCantidad):-
+%     tiene(Persona,Cosa,MayorCantidad),
+%     forall(tiene(Persona,_,Cantidad),MayorCantidad >= Cantidad).
+
+
+% subioA(prudencio, montaniaRusa(hulk)).
+% subioA(prudencio, autosChocadores).
+% subioA(hermenegildo, montaniaRusa(rockit)).
+% subioA(hermenegildo, simulador(minions)).
+% subioA(hermenegildo, autosChocadores).
+% subioA(rigoberta, simulador(ikran)).
+% subioA(brunilda, simulador(simpsons)).
+
+% disfruto(prudencio, montaniaRusa(hulk)).
+% disfruto(prudencio, autosChocadores).
+% disfruto(hermenegildo, simulador(minions)).
+% disfruto(rigoberta, simulador(ikran)).
+
+% laPasoBien(Persona) :- 
+%     subioA(Persona,_),
+%     forall(subioA(Persona, Atraccion), disfruto(Persona, Atraccion)).
+
+% /*
+%     El objetivo del predicado es saber si una persona disfruto en todos los juegos que subio.
+%     Deberia ser cierto para prudencio y rigoberta.
+
+%     El predicado no es inverisble
+
+% */
+
+% esValiente(Persona,Atraccion):- subioA(Persona,montaniaRusa(_)).
+
+toma(juan, coca).
+toma(juan, vino(malbec, 3)).
+toma(daiana, cerveza(golden, 18, 0)).
+toma(gisela, cerveza(ipa, 52, 7)).
+toma(edu, cerveza(stout, 28, 6)).
+
+tieneProblemas(Persona):-
+    findall(C, (toma(Persona, cerveza(C, _, A)), A > 0), Cs),
+    findall(V, toma(Persona, vino(V, _)), Vs),
+    findall(T, toma(Persona, T), Ts),
+    length(Cs, CCs),
+    length(Vs, CVs),
+    length(Ts, CTs),
+    CTs is CCs + CVs.
+
+
+tieneProblemas1(Persona):-
+    toma(Persona,_),
+    not((toma(Persona,Bebida),noTieneAlcohol(Bebida))).
+
+noTieneAlcohol(coca).
+noTieneAlcohol(cerveza(_,_,0)).
+
+/*
+    tieneProblemas(P).
+    false
+    
+    Falso
+    Falso
+    Falso
+
+*/
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # paradigmaLogico
+
+### Declaratividad
+
+Que un **predicado sea declarativo** significa que describe *qué es cierto* sobre el dominio del problema sin especificar *cómo* debe calcularse. En otras palabras, expresa relaciones y condiciones lógicas entre los objetos, dejando que el motor de inferencia del lenguaje (por ejemplo, Prolog) se encargue de buscar las soluciones que satisfacen esas condiciones. Así, el programador se enfoca en **declarar conocimiento** en forma de hechos y reglas, no en dar instrucciones paso a paso como en un lenguaje imperativo.
+
+
